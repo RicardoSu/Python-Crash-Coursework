bb8d08c7258eb018d22b9384a38918f192ef6d38
Ricardo Suarez <ricardolimasuarez@gmail.com>
2020-06-27 01:35:11 -0400

    Add files via upload

---
+++ 8.0 lists.py
@@ -0,0 +1,8 @@
+travel = ['Rome','Cancun', 'London','Miami','Orlando']
+travel.append("Carapicuiba")
+print(travel[-3:])
+print(travel[0:4])
+print(travel[-4:-1])
+print(travel[2:])
+print(travel[-1:])
+print(travel[:-1])
