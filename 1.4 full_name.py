87771dc4cfc40ce08b27ed1979836171bb065606
Ricardo Suarez <ricardolimasuarez@gmail.com>
2020-07-02 16:46:51 -0400

    Delete 1.4 full_name.py

--- 1.4 full_name.py
+++
@@ -1,9 +0,0 @@
-first_name="ada"
-last_name="lovelace"
-full_name = f"{first_name} {last_name}"
-# strings
-print(full_name)
-print(full_name.title())
-print(f"Hello, {full_name.title()}!" )
-message = f"Hello, {full_name.title()}!"
-print(message)
