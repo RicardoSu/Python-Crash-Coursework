1b5f22c3d16c5d00f67db8aae85f757cd39149fd
Ricardo Suarez <ricardolimasuarez@gmail.com>
2020-07-02 16:48:57 -0400

    Delete 1.9  lists.py

--- 1.9  lists.py
+++
@@ -1,8 +0,0 @@
-travel = ['Rome','Cancun', 'London','Miami','Orlando']
-travel.append("Carapicuiba")
-print(travel[-3:])
-print(travel[0:4])
-print(travel[-4:-1])
-print(travel[2:])
-print(travel[-1:])
-print(travel[:-1])
