beb82471c497de634dfc3cc628f651588fa17ec4
Ricardo Suarez <ricardolimasuarez@gmail.com>
2020-07-08 00:31:27 -0400

    Add files via upload

---
+++ 5-1. conditional_test.py
@@ -0,0 +1,12 @@
+car = 'subaru'
+
+print('Is car == "subaru" I predict it is true')
+print(car=='subaru')
+
+print("\n"'Is car == "audi" I predict it is false')
+print(car=='audi')
+
+x=9
+
+print("\n"'Is 9<10 and 9>8? I predict true')
+print(x<10 and x>8)
---
+++ 5-7. favorite_fruit.py
@@ -0,0 +1,8 @@
+fruits = ['apple','banana','peach']
+
+x = 'banana'
+
+for fruit in fruits:
+	if x in fruits:
+		print(f'You really like {x}s!')
+		break
