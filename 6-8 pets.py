6bb189d763c74861d3b6fc361bb32eed26397afc
Ricardo Suarez <ricardolimasuarez@gmail.com>
2020-07-09 14:16:10 -0400

    Add files via upload

---
+++ 6-8 pets.py
@@ -0,0 +1,9 @@
+pets = {
+	'pet_name':['felipe','mario','tamiris'],
+	'pet_owner':['flores','armario','narcia'],
+	'city':['panama','floripa','sao paulo'],
+	'age':['17/7','29/7','20/7']
+}
+
+for key,value in pets.items():
+	print(key, value)
---
+++ 6-9. favorite_places.py
@@ -0,0 +1,8 @@
+fav_places = {
+	'Kody':'Grand Canyon',
+	'David':' Niagara Falls',
+	'Lubna':'Statue of Liberty'
+}
+
+for key,value in fav_places.items():
+	print(f"{key} favorite place is {value}.")
