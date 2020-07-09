cf40cb4d533c887760565fb1ba4a74f6a2e0657f
Ricardo Suarez <ricardolimasuarez@gmail.com>
2020-07-09 00:03:23 -0400

    Add files via upload

---
+++ 5-11. ordinal_numbers .py
@@ -0,0 +1,14 @@
+numbers = [number for number in range(1,10)]
+
+
+for number in numbers:
+	if number == 1:
+		print(f'{number}st', end=', ')
+	elif number == 2:
+		print(f'{number}nd', end=', ')
+	elif number == 3:
+		print(f'{number}rd', end=', ')
+	else:
+		print(f'{number}th', end=', ')
+print('\n')
+	
